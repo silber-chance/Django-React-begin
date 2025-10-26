5784ef75a53fddac852938168aac9bcb805547bb
silber-chance <green19950815.1@gmail.com>
2025-10-26 14:30:50 +0000

    fix frontend index.js add useState

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
